f9d2da6a9eb84bf7027443f12b03fa21b806b150
jsavargas <jsavargas@gmail.com>
2022-02-21 17:36:59 -0300

    fixed

--- src/routes/index.py
+++ src/routes/index.py
@@ -7,6 +7,7 @@ import shutil
 import time
 import json
 import datetime  
+import sqlite3
 
 
 
@@ -16,6 +17,9 @@ from bs4 import BeautifulSoup
 import utils.env as env
 
 import utils.utils 
+from utils.log import logger
+import utils.sonarr as sonarr
+import controller.db as database
 
 
 
@@ -31,6 +35,7 @@ from flask import (
 
 
 index = Blueprint("index", __name__)
+con = sqlite3.connect('/config/anime.db')
 
 connection = []
 linksDownloads = []
@@ -43,37 +48,70 @@ def home():
     season = request.args.get('perPage', default = 'season: WINTER', type = str)
 
 
+    logger("home()")
+
+    #Sonarr = (sonarr.Sonarr('http://192.168.0.10:8989', '61db77c5ec5b4bc3be57ab3a3f1e36e2')
+    Sonarr = ( sonarr.Sonarr('http://192.168.0.10:8989', '61db77c5ec5b4bc3be57ab3a3f1e36e2') )
+
+    _root_folders = Sonarr.get_root_folders()
+    _all_tags = Sonarr.get_all_tags()
+    _quality_profile_id = Sonarr.lookup_quality_profile_id()
+
+
+    media_sorted = []
+    respSonarr = [] 
+    #return render_template(
+    #    "index.html",  data=media_sorted,sonarr=respSonarr, root_folders = _root_folders, all_tags = _all_tags, quality_profile_id= _quality_profile_id
+    #)
+
+
 
     response = utils.utils.getSeasonAnimeList()
 
 
-
     datas = json.loads(response.text)
     media = []
+    respSonarr = []
 
     for data in datas['data']['Page']['media']:
         print(f'page_source => while True  => {json.dumps(data, indent=4)} ', flush=True)
         try:
+            results = []
+            #results = Sonarr.lookup_series(data['title']['english'] if data['title']['english'] is not None else data['title']['romaji']) 
+            
             media.append( {
+                "id": data['id'],
                 "idMal": f"https://myanimelist.net/anime/{data['idMal']}/",
                 "name": data['title']['english'] if data['title']['english'] is not None else data['title']['romaji'],
+                "name_format": (data['title']['english']).replace("'","") if data['title']['english'] is not None else (data['title']['romaji']).replace("'",""),
                 "romaji": data['title']['romaji'],
                 "img_src": data['coverImage']['extraLarge'],
                 "airing" : datetime.datetime.fromtimestamp( data['nextAiringEpisode']['airingAt'] )  ,
-                "airing_mod" : datetime.datetime.fromtimestamp( data['nextAiringEpisode']['airingAt'] ).strftime( "%Y-%m-%d")  ,
+                "airingAt" : datetime.datetime.fromtimestamp( data['nextAiringEpisode']['airingAt'] ).strftime( "%Y-%m-%d")  ,
+                "airingAt" : datetime.datetime.fromtimestamp( data['nextAiringEpisode']['airingAt'] ).strftime( "%Y-%m-%d")  ,
+                "nextAiringEpisode":data['nextAiringEpisode']['episode'],
                 "format":data['format'],
                 "description":data['description'],
                 "hashtag":data['hashtag'],
                 "season":data['season'],
-                "next_episode":data['nextAiringEpisode']['episode'],
                 "status":data['status'],
                 "episodes":data['episodes'],
                 "startDate":data['startDate'],
                 "endDate":data['endDate'],
+                "sonarr": results
             } )
+
+            #logger(json.dumps(results, indent=4))
+
         except:
             continue
 
+    
+    print(f'page_source => while True  => {datetime.date.today()} ', flush=True)
+    print(f"page_source => while True  => {int(datetime.datetime.now().timestamp())} ", flush=True)
+    print(f"page_source => while True  => {int(datetime.datetime.now().timestamp()) + int('494325')} ", flush=True)
+
+    
     media_sorted = sorted(
         media, key=lambda x:( x["airing"] is None ,x["airing"] == '' , x["airing"],  x["status"]), reverse=False
     )
@@ -82,7 +120,7 @@ def home():
     #data = list(dict.fromkeys(data))
 
     return render_template(
-        "index.html",  data=media_sorted
+        "index.html",  data=media_sorted,sonarr=respSonarr, root_folders = _root_folders, all_tags = _all_tags, quality_profile_id= _quality_profile_id
     )
     return response.text
 
@@ -133,6 +171,97 @@ def anime():
         "index.html",  data=media_sorted
     )
     return response.text
+
+
+
+@index.route("/sonarr")
+def seachSonarr():
+
+    search = request.args.get('search', default = 'My Dress-Up Darling', type = str)
+
+    Sonarr = ( sonarr.Sonarr('http://192.168.0.10:8989', '61db77c5ec5b4bc3be57ab3a3f1e36e2') )
+
+    results = Sonarr.lookup_series(search) 
+
+
+    return json.dumps(results)
+
+
+
+
+@index.route("/add",methods=['POST'])
+def add():
+
+    title = request.form.get('title', None)
+    apellido = request.form['inputSeries-127050']
+
+
+    return apellido
+    return request.form
+
+
+
+
+
+@index.route("/update")
+def update():
+
+    page = request.args.get('page', default = 1, type = int)
+    perPage = request.args.get('perPage', default = 50, type = int)
+    year = request.args.get('year', default = datetime.datetime.now().year, type = int)
+
+    database.createTable()
+    return 'media_sorted'
+
+    response = utils.utils.getYearAnimeList(page=page,year=year)
+
+
+
+    datas = json.loads(response.text)
+    media = []
+
+    for data in datas['data']['Page']['media']:
+        print(f'page_source => while True  => {json.dumps(data, indent=4)} ', flush=True)
+        try:
+            media.append( {
+                "idMal": f"https://myanimelist.net/anime/{data['idMal']}/",
+                "name": data['title']['english'] if data['title']['english'] is not None else data['title']['romaji'],
+                "romaji": data['title']['romaji'],
+                "img_src": data['coverImage']['extraLarge'],
+                "airing" : datetime.datetime.fromtimestamp( data['nextAiringEpisode']['airingAt'] ) if data['nextAiringEpisode'] is not None else '' ,
+                "airing_mod" : datetime.datetime.fromtimestamp( data['nextAiringEpisode']['airingAt'] ).strftime( "%Y-%m-%d") if data['nextAiringEpisode'] is not None else '' ,
+                "format":data['format'],
+                "description":data['description'],
+                "hashtag":data['hashtag'],
+                "season":data['season'],
+                "next_episode":data['nextAiringEpisode']['episode'] if data['nextAiringEpisode'] is not None else '',
+                "status":data['status'],
+                "episodes":data['episodes'],
+                "startDate":data['startDate'],
+                "endDate":data['endDate'],
+            } )
+        except:
+            continue
+
+ 
+    media_sorted = sorted(
+        media, key=lambda x:( x["airing"] is None ,x["airing"] == '' , x["airing"],  x["status"]), reverse=False
+    )
+
+
+    return render_template(
+        "index.html",  data=media_sorted
+    )
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ src/utils/sonarr.py
@@ -0,0 +1,211 @@
+"""
+Searcharr
+Sonarr & Radarr Telegram Bot
+Sonarr API Wrapper
+By Todd Roberts
+https://github.com/toddrob99/searcharr
+"""
+import requests
+import time
+from urllib.parse import quote
+
+from utils.log import logger
+
+
+class Sonarr(object):
+    def __init__(self, api_url, api_key, verbose=False):
+        self.logger = logger
+        self.logger("Logging started!")
+        if api_url[-1] == "/":
+            api_url = api_url[:-1]
+        if api_url[:4] != "http":
+            self.logger.error(
+                "Invalid Sonarr URL detected. Please update your settings to include http:// or https:// on the beginning of the URL."
+            )
+        self.api_url = api_url + "/api/{endpoint}?apikey=" + api_key
+        self._all_series = {}
+        self.get_all_series()
+
+    def lookup_series(self, title=None, tvdb_id=None):
+        r = self._api_get(
+            "series/lookup", {"term": f"tvdb:{tvdb_id}" if tvdb_id else quote(title)}
+        )
+        if not r:
+            return []
+
+        return [
+            {
+                "title": x.get("title"),
+                "seasonCount": x.get("seasonCount", 0),
+                "status": x.get("status", "Unknown Status"),
+                "overview": x.get("overview", "Overview not available."),
+                "network": x.get("network"),
+                "remotePoster": x.get(
+                    "remotePoster",
+                    "https://artworks.thetvdb.com/banners/images/missing/movie.jpg",
+                ),
+                "year": x.get("year"),
+                "tvdbId": x.get("tvdbId"),
+                "seriesType": x.get("seriesType"),
+                "imdbId": x.get("imdbId"),
+                "certification": x.get("certification"),
+                "id": x.get("id", self._series_internal_id(x.get("tvdbId"))),
+                "titleSlug": x.get("titleSlug"),
+                "cleanTitle": x.get("cleanTitle"),
+                "tvRageId": x.get("tvRageId"),
+                "images": x.get("images"),
+                "seasons": x.get("seasons"),
+                "profileId": x.get("profileId"),
+            }
+            for x in r
+        ]
+
+    def _series_internal_id(self, tvdb_id):
+        return next(
+            (x["id"] for x in self.get_all_series() if x.get("tvdbId", 0) == tvdb_id),
+            None,
+        )
+
+    def get_all_series(self):
+        if int(round(self._all_series.get("ts", 0))) < int(round(time.time())) - 30:
+            self.logger("Refreshing all series cache...")
+            r = self._api_get("series", {})
+            self._all_series.update({"series": r, "ts": time.time()})
+
+        return self._all_series["series"]
+
+    def add_series(
+        self,
+        series_info=None,
+        tvdb_id=None,
+        path=None,
+        quality=None,
+        search=True,
+        season_folders=True,
+        monitored=True,
+        unmonitor_existing=True,
+        tag=None,
+    ):
+        if not series_info and not tvdb_id:
+            return False
+
+        if not series_info:
+            series_info = self.lookup_series(tvdb_id=tvdb_id)
+            if len(series_info):
+                series_info = series_info[0]
+            else:
+                return False
+
+        params = {
+            "tvdbId": series_info["tvdbId"],
+            "title": series_info["title"],
+            "qualityProfileId": quality,
+            "titleSlug": series_info["titleSlug"],
+            "images": series_info["images"],
+            "seasons": series_info["seasons"],
+            "rootFolderPath": path,
+            "tvRageId": series_info["tvRageId"],
+            "seasonFolder": season_folders,
+            "monitored": monitored,
+            "addOptions": {
+                "ignoreEpisodesWithFiles": unmonitor_existing,
+                "ignoreEpisodesWithoutFiles": "false",
+                "searchForMissingEpisodes": search,
+            },
+        }
+        if tag:
+            if tag_id := self.get_tag_id(tag):
+                params.update({"tags": [tag_id]})
+            else:
+                self.logger.warning(
+                    "Tag lookup/creation failed. The series will not be tagged."
+                )
+
+        return self._api_post("series", params)
+
+    def get_root_folders(self):
+        r = self._api_get("RootFolder", {})
+        if not r:
+            return []
+
+        return [
+            {
+                "path": x.get("path"),
+                "freeSpace": x.get("freeSpace"),
+                "totalSpace": x.get("totalSpace"),
+                "id": x.get("id"),
+            }
+            for x in r
+        ]
+
+    def get_all_tags(self):
+        r = self._api_get("tag", {})
+        self.logger(f"Result of API call to get all tags: {r}")
+        return [] if not r else r
+
+    def add_tag(self, tag):
+        params = {
+            "label": tag,
+        }
+        t = self._api_post("tag", params)
+        self.logger(f"Result of API call to add tag: {t}")
+        return t
+
+    def get_tag_id(self, tag):
+        if i := next(
+            iter(
+                [
+                    x.get("id")
+                    for x in self.get_all_tags()
+                    if x.get("label").lower() == tag.lower()
+                ]
+            ),
+            None,
+        ):
+            self.logger(f"Found tag id [{i}] for tag [{tag}]")
+            return i
+        else:
+            self.logger(f"No tag id found for [{tag}]; adding...")
+            t = self.add_tag(tag)
+            if not isinstance(t, dict):
+                self.logger.error(
+                    f"Wrong data type returned from Sonarr API when attempting to add tag [{tag}]. Expected dict, got {type(t)}."
+                )
+                return None
+            else:
+                self.logger(
+                    f"Created tag id for tag [{tag}]: {t['id']}"
+                    if t.get("id")
+                    else f"Could not add tag [{tag}]"
+                )
+            return t.get("id", None)
+
+    def lookup_quality_profile_id(self):
+        # Look up quality profile id from a profile name,
+        # Or validate existence of a quality profile id
+        r = self._api_get("profile", {})
+        if not r:
+            return 0
+        return [] if not r else r
+
+        return next((x["id"] for x in r if str(v) in [x["name"], str(x["id"])]), 0)
+
+    def _api_get(self, endpoint, params={}):
+        url = self.api_url.format(endpoint=endpoint)
+        for k, v in params.items():
+            url += f"&{k}={v}"
+        self.logger(f"Submitting GET request: [{url}]")
+        r = requests.get(url)
+        if r.status_code not in [200, 201, 202, 204]:
+            r.raise_for_status()
+        else:
+            return r.json()
+
+    def _api_post(self, endpoint, params={}):
+        url = self.api_url.format(endpoint=endpoint)
+        self.logger(f"Submitting POST request: [{url}]; params: [{params}]")
+        r = requests.post(url, json=params)
+        if r.status_code not in [200, 201, 202, 204]:
+            r.raise_for_status()
+        else:
+            return r.json()
